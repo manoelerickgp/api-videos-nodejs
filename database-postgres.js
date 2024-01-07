9320bd78c2a28a807a6c6994e2a3c1fdab9692d1
manoelerickgp <manoel.computacao@gmail.com>
2024-01-07 00:16:55 -0300

    adding database postgres

---
+++ database-postgres.js
@@ -0,0 +1,35 @@
+import { randomUUID } from 'node:crypto'
+import { sql } from './db.js'
+
+export class DatabasePostgres {
+
+    async list() {
+        let videos
+
+        if (search) {
+            videos = await sql`select * from videos where title like "%${search}%"`
+        } else {
+            videos = await sql`select * from videos`
+        }
+    }
+
+    findById(id) {
+       
+    }
+
+    async create(video) {
+        const videoId = randomUUID
+
+        const { title, description, duration } = video
+
+        await sql`insert into videos (id, title, description, duration) VALUES (${videoId}, ${title}, ${description}, ${duration})`
+    }
+
+    update(id, video) {
+        
+    }
+
+    delete(id) {
+        
+    }
+}
